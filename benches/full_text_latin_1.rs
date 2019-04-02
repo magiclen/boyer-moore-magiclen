a33da71673f6757d765bea212f6c1466b04df3d4
Magic Len <len@magiclen.org>
2019-04-02 19:29:37 +0800

    add the needle crate for benchmark reference

--- benches/full_text_latin_1.rs
+++ benches/full_text_latin_1.rs
@@ -2,14 +2,15 @@ extern crate boyer_moore_magiclen;
 #[macro_use]
 extern crate criterion;
 extern crate regex;
+extern crate needle;
 
-mod search;
+mod full_text_search;
 
 use std::fs;
 
 use criterion::Criterion;
 
-use search::*;
+use full_text_search::*;
 
 #[cfg(windows)]
 const TXT_PATH: &'static str = r"benches\data\vgilante.txt";
@@ -59,6 +60,30 @@ fn short_regex(c: &mut Criterion) {
     });
 }
 
+fn short_bm(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("short_bm", move |b| {
+        b.iter(|| {
+            let result = bm_search(&text, PATTERN_SHORT);
+
+            assert_eq!(PATTERN_SHORT_RESULT_COUNT, result.len());
+        })
+    });
+}
+
+fn short_horspool(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("short_horspool", move |b| {
+        b.iter(|| {
+            let result = horspool_search(&text, PATTERN_SHORT);
+
+            assert_eq!(PATTERN_SHORT_RESULT_COUNT, result.len());
+        })
+    });
+}
+
 fn short_latin_1(c: &mut Criterion) {
     let text = fs::read_to_string(TXT_PATH).unwrap();
 
@@ -79,14 +104,14 @@ fn short_character(c: &mut Criterion) {
 
     c.bench_function("short_character", move |b| {
         b.iter(|| {
-            let result = character_search(&text, &pattern);
+            let result = character_search_char(&text, &pattern);
 
             assert_eq!(PATTERN_SHORT_RESULT_COUNT, result.len());
         })
     });
 }
 
-criterion_group!(short, short_native, short_regex, short_latin_1, short_character);
+criterion_group!(short, short_native, short_regex, short_bm, short_horspool, short_latin_1, short_character);
 
 
 fn long_native(c: &mut Criterion) {
@@ -113,6 +138,30 @@ fn long_regex(c: &mut Criterion) {
     });
 }
 
+fn long_bm(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("long_bm", move |b| {
+        b.iter(|| {
+            let result = bm_search(&text, PATTERN_LONG);
+
+            assert_eq!(PATTERN_LONG_RESULT_COUNT, result.len());
+        })
+    });
+}
+
+fn long_horspool(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("long_horspool", move |b| {
+        b.iter(|| {
+            let result = horspool_search(&text, PATTERN_LONG);
+
+            assert_eq!(PATTERN_LONG_RESULT_COUNT, result.len());
+        })
+    });
+}
+
 fn long_latin_1(c: &mut Criterion) {
     let text = fs::read_to_string(TXT_PATH).unwrap();
 
@@ -133,14 +182,14 @@ fn long_character(c: &mut Criterion) {
 
     c.bench_function("long_character", move |b| {
         b.iter(|| {
-            let result = character_search(&text, &pattern);
+            let result = character_search_char(&text, &pattern);
 
             assert_eq!(PATTERN_LONG_RESULT_COUNT, result.len());
         })
     });
 }
 
-criterion_group!(long, long_native, long_regex, long_latin_1, long_character);
+criterion_group!(long, long_native, long_regex, long_bm, long_horspool, long_latin_1, long_character);
 
 fn not_exist_short_native(c: &mut Criterion) {
     let text = fs::read_to_string(TXT_PATH).unwrap();
@@ -160,6 +209,30 @@ fn not_exist_short_regex(c: &mut Criterion) {
     c.bench_function("not_exist_short_regex", move |b| {
         b.iter(|| {
             let result = regex_search(&text, NOT_EXIST_PATTERN_SHORT);
+
+            assert_eq!(0, result.len());
+        })
+    });
+}
+
+fn not_exist_short_bm(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("not_exist_short_bm", move |b| {
+        b.iter(|| {
+            let result = bm_search(&text, NOT_EXIST_PATTERN_SHORT);
+
+            assert_eq!(0, result.len());
+        })
+    });
+}
+
+fn not_exist_short_horspool(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("not_exist_short_horspool", move |b| {
+        b.iter(|| {
+            let result = horspool_search(&text, NOT_EXIST_PATTERN_SHORT);
 
             assert_eq!(0, result.len());
         })
@@ -186,14 +259,14 @@ fn not_exist_short_character(c: &mut Criterion) {
 
     c.bench_function("not_exist_short_character", move |b| {
         b.iter(|| {
-            let result = character_search(&text, &pattern);
+            let result = character_search_char(&text, &pattern);
 
             assert_eq!(0, result.len());
         })
     });
 }
 
-criterion_group!(not_exist_short, not_exist_short_native, not_exist_short_regex, not_exist_short_latin_1, not_exist_short_character);
+criterion_group!(not_exist_short, not_exist_short_native, not_exist_short_regex, not_exist_short_bm, not_exist_short_horspool, not_exist_short_latin_1, not_exist_short_character);
 
 fn not_exist_long_native(c: &mut Criterion) {
     let text = fs::read_to_string(TXT_PATH).unwrap();
@@ -213,6 +286,30 @@ fn not_exist_long_regex(c: &mut Criterion) {
     c.bench_function("not_exist_long_regex", move |b| {
         b.iter(|| {
             let result = regex_search(&text, NOT_EXIST_PATTERN_LONG);
+
+            assert_eq!(0, result.len());
+        })
+    });
+}
+
+fn not_exist_long_bm(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("not_exist_long_bm", move |b| {
+        b.iter(|| {
+            let result = bm_search(&text, NOT_EXIST_PATTERN_LONG);
+
+            assert_eq!(0, result.len());
+        })
+    });
+}
+
+fn not_exist_long_horspool(c: &mut Criterion) {
+    let text = fs::read_to_string(TXT_PATH).unwrap();
+
+    c.bench_function("not_exist_long_horspool", move |b| {
+        b.iter(|| {
+            let result = horspool_search(&text, NOT_EXIST_PATTERN_LONG);
 
             assert_eq!(0, result.len());
         })
@@ -239,13 +336,13 @@ fn not_exist_long_character(c: &mut Criterion) {
 
     c.bench_function("not_exist_long_character", move |b| {
         b.iter(|| {
-            let result = character_search(&text, &pattern);
+            let result = character_search_char(&text, &pattern);
 
             assert_eq!(0, result.len());
         })
     });
 }
 
-criterion_group!(not_exist_long, not_exist_long_native, not_exist_long_regex, not_exist_long_latin_1, not_exist_long_character);
+criterion_group!(not_exist_long, not_exist_long_native, not_exist_long_regex, not_exist_long_bm, not_exist_long_horspool, not_exist_long_latin_1, not_exist_long_character);
 
 criterion_main!(short, long, not_exist_short, not_exist_long);
--- benches/full_text_search/mod.rs
+++ benches/full_text_search/mod.rs
@@ -1,10 +1,13 @@
 extern crate regex;
 extern crate boyer_moore_magiclen;
+extern crate needle;
 
 mod utf8_width;
 
 use self::regex::Regex;
 use self::boyer_moore_magiclen::{BMLatin1BadCharShiftMap, BMCharacterBadCharShiftMap};
+
+use self::needle::{Horspool, BoyerMoore};
 
 pub fn native_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<usize> {
     let text = text.as_ref();
@@ -62,6 +65,24 @@ pub fn regex_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<us
     result
 }
 
+pub fn bm_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<usize> {
+    let text = text.as_ref();
+    let pattern = pattern.as_ref();
+
+    let needle = BoyerMoore::new(pattern.as_bytes());
+
+    needle.find_in(text.as_bytes()).collect()
+}
+
+pub fn horspool_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<usize> {
+    let text = text.as_ref();
+    let pattern = pattern.as_ref();
+
+    let needle = Horspool::new(pattern.as_bytes());
+
+    needle.find_in(text.as_bytes()).collect()
+}
+
 pub fn latin_1_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<usize> {
     let pattern = pattern.as_ref();
 
@@ -70,7 +91,7 @@ pub fn latin_1_search<S: AsRef<str>, P: AsRef<str>>(text: S, pattern: P) -> Vec<
     boyer_moore_magiclen::latin_1::find_in_str(text, pattern.as_bytes(), &bad_char_shift_map, 0)
 }
 
-pub fn character_search(text: &[char], pattern: &[char]) -> Vec<usize> {
+pub fn character_search_char(text: &[char], pattern: &[char]) -> Vec<usize> {
     let bad_char_shift_map = BMCharacterBadCharShiftMap::create_bad_char_shift_map_from_char_slice(pattern).unwrap();
 
     boyer_moore_magiclen::character::find_in_char_slice(text, pattern, &bad_char_shift_map, 0)
